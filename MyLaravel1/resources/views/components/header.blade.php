aaf596214615d78b88865916c891450fcb70fceb
Simone <marzolla.istruzione@gmail.com>
2021-05-18 17:44:20 +0200

    Start

---
+++ MyLaravel1/resources/views/components/header.blade.php
@@ -0,0 +1,67 @@
+<header>
+  <div id="min-header">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            DC POWER VISA &reg;
+          </li>
+          <li>
+            ADDITIONAL DC SITES
+            <i class="fas fa-sort-down"></i>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div id="max-header">
+    <div class="container">
+      <div class="flexi">
+        <div id="logo">
+          <img src="#none" alt="">
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+              <a href="#">COMICS</a>
+            </li>
+            <li>
+              <a href="#">MOVIES</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">GAMES</a>
+            </li>
+            <li>
+              <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+              <a href="#">VIDEOS</a>
+            </li>
+            <li>
+              <a href="#">FANS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              SHOP
+              <i class="fas fa-sort-down"></i>
+            </li>
+          </ul>
+        </nav>
+        <div id="search">
+          <input type="search" name="" value="" placeholder="Search...">
+          <i class="fas fa-search"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="jumbotron">
+  </div>
+</header>
--- MyLaravel1/resources/views/layouts/main-layout.blade.php
+++ MyLaravel1/resources/views/layouts/main-layout.blade.php
@@ -3,6 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- GOOGLE FONT - MUKTA -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@300&display=swap" rel="stylesheet">
+    <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
   </head>
   <body>
