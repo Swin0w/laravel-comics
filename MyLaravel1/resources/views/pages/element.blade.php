eed1726df2f91c47da7b772ae8f770656edc30b1
Simone <marzolla.istruzione@gmail.com>
2021-05-20 17:43:01 +0200

    fix

--- MyLaravel1/resources/views/pages/element.blade.php
+++ MyLaravel1/resources/views/pages/element.blade.php
@@ -2,17 +2,19 @@
 @section('content')
 <div class="jumbotron">
 </div>
-<div class="container">
-    <div id="desc-poster">
-        <div id="desc">
-            <p>{{ $elem['description'] }}</p>
-            <div id="prezzo">
-                <p>{{ $elem['price']}}</p>
+<main>
+    <div class="container">
+        <div id="desc-poster">
+            <div id="desc">
+                <p>{{ $elem['description'] }}</p>
+                <div id="prezzo">
+                    <p>{{ $elem['price']}}</p>
+                </div>
+            </div>
+            <div id="poster">
+                <img src="{{ $elem['thumb'] }}" alt="">
             </div>
         </div>
-        <div id="poster">
-            <img src="{{ $elem['thumb'] }}" alt="">
-        </div>
     </div>
-</div>
+</main>
 @endsection
